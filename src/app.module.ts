20bf3eb7a03c0cd62cdd6977d35d7770b2a999a6
Douglas Volcato <douglasvolcato@gmail.com>
2023-12-14 00:10:34 -0300

    chore: add project setup

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as any,
+      host: process.env.PG_HOST as any,
+      port: process.env.PG_PORT as any,
+      username: process.env.PG_USER as any,
+      password: process.env.PG_PASSWORD as any,
+      database: process.env.PG_DB as any,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
