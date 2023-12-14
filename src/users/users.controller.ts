88dff1c37fd0bbd90f50637f027ade59bcfb4f5b
Douglas Volcato <douglasvolcato@gmail.com>
2023-12-14 00:10:52 -0300

    feat: add user module

---
+++ src/users/users.controller.ts
@@ -0,0 +1,46 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Delete,
+  Put,
+  Param,
+  Body,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Get()
+  async findAll(): Promise<User[]> {
+    return await this.userService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: number): Promise<User> {
+    const user = await this.userService.findOne(id);
+    if (!user) {
+      throw new Error('User not found');
+    } else {
+      return user;
+    }
+  }
+
+  @Post()
+  async create(@Body() user: User): Promise<User> {
+    return await this.userService.create(user);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: number, @Body() user: User): Promise<User> {
+    return await this.userService.update(id, user);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: number): Promise<void> {
+    return await this.userService.delete(id);
+  }
+}
